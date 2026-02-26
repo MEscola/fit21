7a0e34f32b8efb2708d09985fb6a8bd3b931a8ab
Marcia <marcia.escolastico@gmail.com>
2026-02-26 14:30:33 -0300

    first commit

---
+++ src/main/java/com/project/fit21/Fit21Application.java
@@ -0,0 +1,13 @@
+package com.project.fit21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fit21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fit21Application.class, args);
+	}
+
+}
